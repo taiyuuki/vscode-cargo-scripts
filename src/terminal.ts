7ff3f260fbc751b597cb608f310598b7ef118414
taiyuuki <taiyuuki@qq.com>
2023-04-06 18:36:08 +0800

    fix: Terminals

--- src/terminal.ts
+++ src/terminal.ts
@@ -1,29 +1,24 @@
 import path from 'path'
 import * as vscode from 'vscode'
 
-const terminals = new Map()
-
-vscode.window.onDidCloseTerminal((terminal) => {
-  if (terminals.has(terminal.name)) {
-    terminals.delete(terminal.name)
-  }
-})
-
 export function executeCommand(cmd: string, label: string, cwd: string) {
   let terminal: vscode.Terminal
   const separate = vscode.workspace.getConfiguration().get('cargoScripts.terminal')
   const terminalName = separate ? `${path.basename(cwd)}: ${label}` : path.basename(cwd)
-  const terminalOptions: vscode.TerminalOptions = {
-    name: terminalName,
-    cwd,
-    hideFromUser: false,
-  }
-  if (terminals.has(terminalName)) {
-    terminal = terminals.get(terminalName)
+  const findTerminal = vscode.window.terminals.find(ter => {
+    return ter.name === terminalName
+  })
+  if (findTerminal) {
+    terminal = findTerminal
   }
   else {
+    const terminalOptions: vscode.TerminalOptions = {
+      name: terminalName,
+      cwd,
+      hideFromUser: false,
+    }
+
     terminal = vscode.window.createTerminal(terminalOptions)
-    terminals.set(terminalName, terminal)
   }
 
   terminal.show()
